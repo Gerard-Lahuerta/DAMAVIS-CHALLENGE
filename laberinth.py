69612d4af8661c28b15fbbcee750eff703baca4c
Gerard Lahuerta <gerard.lahuerta.martin@gmail.com>
2023-07-19 12:46:59 +0200

    Cell class and create labenith method done

--- laberinth.py
+++ laberinth.py
@@ -2,17 +2,19 @@ class Cell():
     '''
     Represents de cells of the laberinth
     '''
-    def __init__(self):
+    def __init__(self, type):
+        self.type = type
         self.rotate = True
         self.neighbors = []
         self.distance = 0
         self.visited = False
+        self.n_visited = 0
 
     def set_distance(self, dist):
         self.distance = dist
     
-    def add_neighbor(self, cell_position):
-        self.neighbors.append(cell_position)
+    def set_neighbor(self, neighbors_position):
+        self.neighbors.append(neighbors_position)
     
     def change_rotate(self):
         self.rotate = False
@@ -20,19 +22,51 @@ class Cell():
     def update_cell(self):
         self.visited = True
 
-    def get_neigbors(self):
-        return self.neighbors
+    def get_neigbor(self):
+        neigh = self.neighbors[self.n_visited//2]
+        return neigh
+    
+    def get_n_visited(self):
+        return self.n_visited
 
+############################################################################################
 
-
-def CalcDistance(): 
+def calc_Manh_distance(pos, pos_exit): 
     '''
     Calculates de Manhattan distance
     '''
-    pass
+    return sum(pos_exit-pos)
+
+def calc_neighbors(pos, n_last_row, n_last_col):
+    neighs = []
+    for x,i in enumerate([1,-1]):
+        for y,j in enumerate([1,-1]):
+            X = pos[x]+i
+            Y = pos[y]+j
+            if X > n_last_row or X < 0 or Y > n_last_col or Y < 0:
+                continue
+            neighs.append([X,Y])
+    return neighs
+
+def create_laberinth_cell(lab):
+    n_last_row = len(lab)-1
+    n_last_col = len(lab[-1])-1
+
+    pos_exit = [n_last_row, n_last_col]
+
+    for n_row, row in enumerate(lab):
+        for n_col, col in row:
+            cell = Cell(col)
+            pos = [n_row, n_col]
+            dist = calc_Manh_distance(pos, pos_exit)
+
+            neigh = calc_neighbors(pos, n_last_row, n_last_col)
+
+            if col == "#":
+                wall_neigh_pos += neigh
 
 
-def AStar(): 
+def A_Star(): 
     '''
     Runs AStar algorithm to find the shortest path
     '''
@@ -46,4 +80,4 @@ laberinth = [ ['.','.','.','.','.','.','.','.','.'],
               ['.','#','.','.','.','.','.','#','.'] ]
 
 if __name__ == "__main__":
-    pass
+    create_laberinth_cell(laberinth)
