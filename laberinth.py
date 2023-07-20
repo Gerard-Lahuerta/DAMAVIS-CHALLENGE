e20239eaf61ac8eb77df19ab891d051b724d1aed
Gerard Lahuerta <gerard.lahuerta.martin@gmail.com>
2023-07-20 15:16:03 +0200

    End of the class docs

--- laberinth.py
+++ laberinth.py
@@ -4,37 +4,75 @@ DAMAVIS CHALLENGE PROGRAM PROPORSAL
 @Author: Gerard Lahuerta Martín
 @Date: 20/07/2023
 
-The program is separated in classes needed to run the search algorithm, auxiliar fucntions 
-to calculate rellevant data, function that translate the input ( list(list(str)) ) into 
-the structure needed to run the search algorithm and the algorithm.
+The program is separated in classes needed to run the search algorithm, auxiliar fucntions to calculate 
+rellevant data, function that translate the input ( list(list(str)) ) into the structure needed to run the
+search algorithm and the algorithm.
 
-For more information about the algorithms and explanations of the decisions done while 
-programming the program consult the Readme file.
+For more information about the algorithms and explanations of the decisions done while programming consult
+the Readme file.
 '''
 
-############################################################################################
+
+###########################################################################################################
+
 
 class Cell(): 
     '''
-    Class Object that represents de cells of the labyrinth
+    Class Object that represents de cells of the labyrinth.
+
+    It contains the rellevant information obtained in the input "labyinth map". 
+    Also contains important information for the searching algorithm.
+
+    Attributes:
+      - type: character that represents the type of the cell (wall "#" or free ".").
+      - neighbors: list of coordinates of its cells neighbors.
+      - moves: integuer that count the moves done while its in the cell.
+      - axis: character that represents the orientation of the rectangle (horizontal "X" or vertical "Y").
+      - position: lest with the coordinates of the cell.
     '''
     def __init__(self, type, x, y, upper_bound):
+        '''
+        Inicialice the Cell class.
+
+        Args:
+          - type: character "#" or "." that represents the type of teh cell.
+          - x: integuer which represents the row where the cell its found.
+          - y: integuer which represents the column where the cell its found.
+          - upper_bound: integuer which is the maximum number of moves that the rectangle could do.
+
+        Rellevant information:
+          - Attribute axis will be inicialiced in horizontal, "X".
+          - Attribute moves will be inicialiced as infinite. 
+        '''
         self.type = type
         self.neighbors = []
         self.moves = float("inf")
         self.axis = "X"
         self.position = [x,y]
         self.estimated_distance = upper_bound
-
-        self.before = []
     
     def set_neighbors(self, neighbors_position):
+        '''
+        Sets the new neighbors of the cell.
+
+        Args:
+          - neighbors_position: list with the coordinates of the new neighbors.
+        '''
         self.neighbors = neighbors_position
 
     def get_neighbors(self):
+        '''
+        Returns the neighbors of the cell.
+        '''
         return self.neighbors
     
     def rotate(self):
+        '''
+        Update the orientation of the rectangle that is in the cell.
+
+        Rellevant information:
+          - Rotate the rectangle is consider a moves, so the attribute moves will be increased in 1.
+        '''
         if self.axis == "X":
             self.axis = "Y"
         else:
@@ -42,24 +80,72 @@ class Cell():
         self.moves += 1
 
     def get_position(self):
+        '''
+        Returns the position of the cell in the "labyrinth map".
+
+        Return:
+          - position: list
+        '''
         return self.position
 
     def set_estimated_distance(self, dist):
+        '''
+        Updates the estimated moves needed to arribe at the exit.
+
+        Args:
+          - dist: integuer that represents the estimation of moves to complete the labyrinth.
+
+        Rellevant information:
+          - Is important to have the moves updated to register the real estimated distance.
+        '''
         self.estimated_distance = self.moves + dist
 
     def get_moves(self):
+        '''
+        Returns the moves the rectangle have done to arribe at the cell.
+
+        Return:
+          - moves: integuer
+        '''
         return self.moves
     
     def set_moves(self, moves):
+        '''
+        Set the new moves passed to the cell.
+
+        Args:
+          - moves: integuer that represents the new number of moves needed to arribe at the cell.
+        '''
         self.moves = moves
     
     def set_rotation(self, rot):
+        '''
+        Set the new orientation of the rectangle that is the the cell.
+
+        Args:
+          - rot: character that represents the orientation of the rectangle.
+
+        Rellevant information:
+          - rot needs to be "#" or ".".
+        '''
         self.axis = rot
 
     def get_rotation(self):
+        '''
+        Returns the orientation of the rectangle that is in the cell.
+
+        Return:
+          - axis: character
+        '''
         return self.axis
     
     def get_type(self):
+        '''
+        Returns the type of the cell
+
+        Return:
+          - type: character
+        '''
         return self.type
 
 
@@ -219,7 +305,7 @@ def A_Star(start, n_last_row, n_last_col):
 
 
 ############################################################################################
-'''
+
 labyrinth = [ [".",".",".",".",".",".",".",".",".","."],
               [".","#",".",".",".",".","#",".",".","."],
               [".","#",".",".",".",".",".",".",".","."],
@@ -230,7 +316,7 @@ labyrinth = [ [".",".",".",".",".",".",".",".",".","."],
               [".",".",".",".",".",".","#",".",".","."],
               [".",".",".",".",".",".",".",".",".","."],
               [".",".",".",".",".",".",".",".",".","." ] ]
-'''
+
 '''
 labyrinth =[ [".",".","."],
              [".",".","."],
@@ -243,13 +329,13 @@ labyrinth = [ ['.','.','.','.','.','.','.','.','.'],
               ['.','#','.','.','.','.','.','#','.'],
               ['.','#','.','.','.','.','.','#','.'] ]
 '''
-
+'''
 labyrinth = [ ['.','.','.','.','.','.','.','.','.'],
               ['#','.','.','.','#','.','.','.','.'],
               ['.','.','.','.','#','.','.','.','.'],
               ['.','#','.','.','.','.','.','#','.'],
               ['.','#','.','.','.','.','.','#','.'] ]
-
+'''
 
 if __name__ == "__main__":
 
@@ -261,21 +347,5 @@ if __name__ == "__main__":
 
     create_labyrinth_cell(n_last_row, n_last_col)
 
-    for i in labyrinth:
-        line = []
-        for j in i:
-            line.append(j.type)
-        print(line)
-
     print(A_Star(labyrinth[0][1], n_last_row, n_last_col))
-    
-    if len(labyrinth[-2][-1].before) != 0:
-        cell = labyrinth[-2][-1]
-    else :
-        cell = labyrinth[-1][-2]
-    print(cell.position)
-
-    while len(cell.before) != 0:
-        print(cell.before)
-        cell = labyrinth[cell.before[0]][cell.before[1]]
 
