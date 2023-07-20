030d17f7ea102b20195c97faf2666f0a9d10d485
Gerard Lahuerta <gerard.lahuerta.martin@gmail.com>
2023-07-20 14:24:56 +0200

    Cell class cleaned

--- laberinth.py
+++ laberinth.py
@@ -5,8 +5,6 @@ class Cell():
     def __init__(self, type, x, y, upper_bound):
         self.type = type
         self.neighbors = []
-        self.visited = False
-        self.n_visited = 0
         self.moves = float("inf")
         self.axis = "X"
         self.position = [x,y]
@@ -20,52 +18,24 @@ class Cell():
     def get_neighbors(self):
         return self.neighbors
     
-    def change_rotate(self):
-        self.rotate = False
-
-    def update_cell(self):
-        self.visited = True
-
-    def get_neigbor(self):
-        neigh = self.neighbors[self.n_visited//2]
-        return neigh
-    
-    def update_moves(self):
-        self.moves += 1
-    
-    def get_n_visited(self):
-        return self.n_visited
-    
     def rotate(self):
         if self.axis == "X":
             self.axis = "Y"
         else:
             self.axis = "X"
-        self.update_moves()
+        self.moves += 1
 
     def get_position(self):
         return self.position
-    
-    def get_distance(self):
-        return self.distance
-    
-    def set_visited(self):
-        self.visited = True
 
     def set_estimated_distance(self, dist):
         self.estimated_distance = self.moves + dist
 
-    def get_visited(self):
-        return self.visited
-    
     def get_moves(self):
         return self.moves
     
     def set_moves(self, moves):
         self.moves = moves
-    
-    def get_estimate_distance(self):
-        return self.estimated_distance
     
     def set_rotation(self, rot):
         self.axis = rot
@@ -203,7 +173,6 @@ def A_Star(start, n_last_row, n_last_col):
     while len(queue) != 0: # and count < 10:
         cell = queue[0]
         queue = queue[1:]
-        cell.set_visited()
 
         pos = cell.get_position()
         if calc_Manh_distance(pos, pos_exit) == 1:
@@ -243,11 +212,11 @@ labyrinth = [ [".",".",".",".",".",".",".",".",".","."],
               [".",".",".",".",".",".",".",".",".","."],
               [".",".",".",".",".",".",".",".",".","." ] ]
 '''
-
+'''
 labyrinth =[ [".",".","."],
              [".",".","."],
              [".",".","."] ]
-
+'''
 '''
 labyrinth = [ ['.','.','.','.','.','.','.','.','.'],
               ['#','.','.','.','#','.','.','#','.'],
@@ -255,13 +224,13 @@ labyrinth = [ ['.','.','.','.','.','.','.','.','.'],
               ['.','#','.','.','.','.','.','#','.'],
               ['.','#','.','.','.','.','.','#','.'] ]
 '''
-'''
+
 labyrinth = [ ['.','.','.','.','.','.','.','.','.'],
               ['#','.','.','.','#','.','.','.','.'],
               ['.','.','.','.','#','.','.','.','.'],
               ['.','#','.','.','.','.','.','#','.'],
               ['.','#','.','.','.','.','.','#','.'] ]
-'''
+
 
 if __name__ == "__main__":
 
